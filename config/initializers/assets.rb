ca8ab30e0769e493d80a8db5de1f689d80c46db4
Sung Choi <sungyeol.choi@gmail.com>
2016-12-20 15:43:39 -0800

    Connet the ng2-app with rails controller

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,3 +6,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( ng2-app/inline.js ng2-app/styles.bundle.js ng2-app/main.bundle.js)
